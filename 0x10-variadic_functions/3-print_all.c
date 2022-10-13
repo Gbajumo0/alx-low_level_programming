d9a7e67996d4332c9051671018b40cc34aefb62f
Gbajumo Olaleye <charityifeo@gmail.com>
2022-10-13 07:00:32 -0700

    poy

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,100 +1,45 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * op_c - Print character .
- * @form: name va_list
- *
- * Return: Nothing.
- */
-
-void op_c(va_list form)
-{
-	printf("%c", va_arg(form, int));
-}
-/**
- * op_i - Print Integer
- * @form: name va_list
- *
- * Return: Nothing.
- */
-
-void op_i(va_list form)
-{
-	printf("%i", va_arg(form, int));
-}
-/**
- * op_f - print FLoat numbers
- * @form: name of va_list
- *
- * Return: Nothing.
- */
-
-void op_f(va_list form)
-{
-	printf("%f", va_arg(form, double));
-}
-/**
- * op_s -print string
- * @form: name va_list
- *
- * Return: Nothing.
- */
-
-void op_s(va_list form)
-{
-	char *str;
-
-	str = va_arg(form, char *);
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
-/**
- * print_all - check the code for Holberton School students.
- * @format: number of arguments in character format
- *
- * Return: Nothing.
- */
-
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+* Return: no return
+*/
 void print_all(const char * const format, ...)
 {
+	va_list stuff;
+	int i = 0;
+	char *s;
 
-	va_list all;
-	unsigned int i, j;
-	char *separator = "";
+	va_start(stuff, format);
 
-	f ops[] = {
-		{"c", op_c},
-		{"i", op_i},
-		{"f", op_f},
-		{"s", op_s},
-		};
-
-	va_start(all, format);
-	i = 0;
 	while (format && format[i])
 	{
-		j = 0;
-		while (j < 4)
+		switch (format[i++])
 		{
-			if (ops[j].op[0] == format[i])
-			{
-				printf("%s", separator);
-				separator = ", ";
-				ops[j].f(all);
-				break;
-			}
-			j++;
+		case 'c':
+			printf("%c", va_arg(stuff, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(stuff, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(stuff, double));
+			break;
+		case 's':
+			s = va_arg(stuff, char *);
+			if (!s)
+				s = "(nil)";
+			printf("%s", s);
+			break;
+		default:
+			continue;
 		}
-	i++;
+		if (format[i])
+			printf(", ");
 	}
-
 	printf("\n");
-	va_end(all);
+	va_end(stuff);
 }
